ed0b84891d1d0dec255b49f3cc8b11dc81bcb76b
RexVargas <rvargas.itq@gmail.com>
2025-09-11 14:13:52 -0600

    feat: apps and lib created

---
+++ ecommerce-mai-dao/jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  displayName: '@xocho-xuan-tang/ecommerce-mai-dao',
+  preset: '../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../coverage/ecommerce-mai-dao',
+  testEnvironment: 'jsdom',
+};
+
+export default createJestConfig(config);
