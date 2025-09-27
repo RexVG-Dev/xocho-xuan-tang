4fca3c6a16337ae3f59ac6729748253e4ebfe757
RexVargas <rvargas.itq@gmail.com>
2025-09-27 15:50:14 -0600

    feat: implement types and different messages for global loader

--- apps/system-cang-ku/src/app/contexts/LoadingContext.tsx
+++ apps/system-cang-ku/src/app/contexts/LoadingContext.tsx
@@ -6,21 +6,49 @@ import {
   ReactNode
 } from 'react';
 
+export type LoaderType = 'changes' | 'get' | 'login' | 'upload' | 'delete' | 'custom' | 'default';
+
+type ShowLoaderType = {
+  type?: LoaderType;
+  customMessage?: string
+};
 export interface LoadingContextType {
   isLoading: boolean;
-  showLoader: () => void;
+  message: string;
+  showLoader: ({type, customMessage}: ShowLoaderType) => void;
   hideLoader: () => void;
 }
 
 export const LoadingContext = createContext<LoadingContextType | undefined>(undefined);
 
+const messages: Record<LoaderType, string> = {
+  changes: 'Espera un momento, estamos guardando tus cambios.',
+  get: 'Estamos obteniendo tu información.',
+  login: 'Estamos validando tus credenciales.',
+  upload: 'Subiendo tus archivos, por favor espera.',
+  delete: 'Eliminando el registro de forma permanente.',
+  custom: '',
+  default: 'Procesando...',
+};
+
 export function LoadingProvider({ children }: { children: ReactNode }) {
   const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState(messages.default);
 
-  const showLoader = () => setIsLoading(true);
+  const showLoader = ({
+    type = 'default',
+    customMessage
+  }: ShowLoaderType) => {
+    if (type === 'custom' && customMessage) {
+      setMessage(customMessage);
+    } else {
+      setMessage(messages[type]);
+    }
+    setIsLoading(true);
+  };
   const hideLoader = () => setIsLoading(false);
 
-  const value = { isLoading, showLoader, hideLoader };
+  const value = { isLoading, message, showLoader, hideLoader };
 
   return (
     <LoadingContext.Provider value={value}>{children}</LoadingContext.Provider>
