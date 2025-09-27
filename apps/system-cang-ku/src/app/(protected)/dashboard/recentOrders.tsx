3340fd313b78f4ad8e3e82f1033f75a6bdc6c4d3
RexVargas <rvargas.itq@gmail.com>
2025-09-27 17:44:40 -0600

    feat: implement basic structure

---
+++ apps/system-cang-ku/src/app/(protected)/dashboard/recentOrders.tsx
@@ -0,0 +1,11 @@
+import { Card } from "@/app/components/ui"
+
+function RecentOrders() {
+  return (
+    <Card title="Órdenes recientes" className="bg-white">
+      <div>RecentOrders</div>
+    </Card>
+    
+  )
+}
+export default RecentOrders
---
+++ apps/system-cang-ku/src/app/(protected)/dashboard/summary.tsx
@@ -0,0 +1,11 @@
+import { Card } from "@/app/components/ui"
+
+function Summary() {
+  return (
+    <Card title="Resumen" className="bg-white">
+      <div>Summary</div>
+    </Card>
+    
+  )
+}
+export default Summary
